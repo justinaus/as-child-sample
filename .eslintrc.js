8ace7ff4d0a1cb1ec626dad1eacc7d96def220dc
Justin Koo <justinaus@Justins-MacBook-Pro.local>
2024-04-23 20:51:02 +0900

    Fix eslint - prettier warn

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,6 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-unused-vars': 'warn',
     '@typescript-eslint/no-explicit-any': 'warn',
+    'prettier/prettier': 'warn',
   },
 };
